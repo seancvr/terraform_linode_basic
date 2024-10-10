20ce671b8c85237ce6e11996b60c17eba8394b35
seancvr <134600598+seancvr@users.noreply.github.com>
2024-10-10 14:49:31 -0600

    Update set_vars.sh

--- set_vars.sh
+++ set_vars.sh
@@ -9,6 +9,7 @@ response=$(curl --request GET \
 # parse firewall id from response
 firewall_id=$(echo $response | jq -r '.data[0].id')
 
+# set environment variables
 export TF_VAR_linode_API_token="$(<~/.API_tokens/linode_terraform_token)"
 export TF_VAR_ssh_public_key="$(<~/.ssh/id_ed25519.pub)"
 export TF_VAR_root_password="$(openssl rand --base64 16)"
